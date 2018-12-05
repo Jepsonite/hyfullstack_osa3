f9631641ed0ed983439131d8577bd5bc577b53ff
Tuomas Manninen <tuomas.manninen@codento.com>
2018-12-05 09:17:48 +0200

    frontti mukaan

---
+++ build/precache-manifest.35b0337e2a2b23e43a42b410e2f995d5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "05a2f4f72ed9afa7c871",
+    "url": "/static/js/main.05a2f4f7.chunk.js"
+  },
+  {
+    "revision": "3fc2ca5e2c1e056a460e",
+    "url": "/static/js/1.3fc2ca5e.chunk.js"
+  },
+  {
+    "revision": "05a2f4f72ed9afa7c871",
+    "url": "/static/css/main.7c9a2495.chunk.css"
+  },
+  {
+    "revision": "e7fcd41880182a24c915c69ddaf5daab",
+    "url": "/index.html"
+  }
+];
